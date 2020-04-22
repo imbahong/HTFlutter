9e61075110121a9d84957476d160553ca90b7e9f
hong <imbaTao@outlook.com>
2020-04-22 08:50:50 +0800

    轮播图插件

--- my_app/lib/Main/Tabbar/Home/Home.dart
+++ my_app/lib/Main/Tabbar/Home/Home.dart
@@ -1,14 +1,30 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'dart:ui';
+import 'package:flutter_swiper/flutter_swiper.dart';
+// import 'package:cached_network_image/cached_network_image.dart';
 
 class Home extends StatelessWidget {
+   List swiperDataList=["https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1562723625662&di=bc7be59dd27706ea65ea33a94c209477&imgtype=0&src=http%3A%2F%2Fpic.58pic.com%2F58pic%2F12%2F40%2F43%2F18958PICYpQ.jpg",
+  "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1562723734494&di=864f7b85f900f0b68e8bc08f1c078eed&imgtype=0&src=http%3A%2F%2Fb-ssl.duitang.com%2Fuploads%2Fblog%2F201511%2F02%2F20151102140204_WUSwE.jpeg",
+  "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1562723821634&di=e04d14690229411a560ccc6cf0e10f6a&imgtype=0&src=http%3A%2F%2Fpic.58pic.com%2F58pic%2F15%2F01%2F96%2F56N58PICVWw_1024.jpg"];
+
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
         children: <Widget>[
           HomeNavgaionBar(),
+          Swiper(
+            outer: false,
+            itemBuilder: (c,i) {
+              if(swiperDataList != null) {
+                  
+              }
+            },
+          )
         ],
       ),
       floatingActionButton: FloatingActionButton(
@@ -20,6 +36,10 @@ class Home extends StatelessWidget {
   }
 }
 
+
+
+
+// 导航栏按钮
 class HTNavigationButtonItem extends StatefulWidget {
   @override
   _NavigationButtonItemState createState() => _NavigationButtonItemState();
