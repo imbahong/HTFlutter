0228ac55647041c9cad192a34fec06f1b4773cc9
hong <imbaTao@outlook.com>
2020-04-23 08:13:09 +0800

    轮播图控件加载成功

--- my_app/lib/Main/Tabbar/Home/Home.dart
+++ my_app/lib/Main/Tabbar/Home/Home.dart
@@ -2,14 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'dart:ui';
 import 'package:flutter_swiper/flutter_swiper.dart';
-// import 'package:cached_network_image/cached_network_image.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 
 class Home extends StatelessWidget {
-   List swiperDataList=["https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1562723625662&di=bc7be59dd27706ea65ea33a94c209477&imgtype=0&src=http%3A%2F%2Fpic.58pic.com%2F58pic%2F12%2F40%2F43%2F18958PICYpQ.jpg",
-  "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1562723734494&di=864f7b85f900f0b68e8bc08f1c078eed&imgtype=0&src=http%3A%2F%2Fb-ssl.duitang.com%2Fuploads%2Fblog%2F201511%2F02%2F20151102140204_WUSwE.jpeg",
-  "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1562723821634&di=e04d14690229411a560ccc6cf0e10f6a&imgtype=0&src=http%3A%2F%2Fpic.58pic.com%2F58pic%2F15%2F01%2F96%2F56N58PICVWw_1024.jpg"];
-
-
+  List swiperDataList = [
+    "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1562723625662&di=bc7be59dd27706ea65ea33a94c209477&imgtype=0&src=http%3A%2F%2Fpic.58pic.com%2F58pic%2F12%2F40%2F43%2F18958PICYpQ.jpg",
+    "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1562723734494&di=864f7b85f900f0b68e8bc08f1c078eed&imgtype=0&src=http%3A%2F%2Fb-ssl.duitang.com%2Fuploads%2Fblog%2F201511%2F02%2F20151102140204_WUSwE.jpeg",
+    "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1562723821634&di=e04d14690229411a560ccc6cf0e10f6a&imgtype=0&src=http%3A%2F%2Fpic.58pic.com%2F58pic%2F15%2F01%2F96%2F56N58PICVWw_1024.jpg"
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -17,14 +17,29 @@ class Home extends StatelessWidget {
       body: Column(
         children: <Widget>[
           HomeNavgaionBar(),
-          Swiper(
-            outer: false,
-            itemBuilder: (c,i) {
-              if(swiperDataList != null) {
-                  
-              }
-            },
-          )
+          ConstrainedBox(
+              child: Swiper(
+                outer: false,
+                autoplay: true,
+                autoplayDelay: 15000,
+                duration: 500,
+                itemBuilder: (c, i) {
+                  if (swiperDataList != null) {
+                    return CachedNetworkImage(
+                      imageUrl: "${swiperDataList[i]}",
+                      placeholder: (context, url) =>
+                          new CircularProgressIndicator(),
+                      errorWidget: (context, url, error) =>
+                          new Icon(Icons.error),
+                    );
+                  }
+                },
+                pagination:
+                    new SwiperPagination(margin: new EdgeInsets.all(5.0)),
+                itemCount: swiperDataList == null ? 0 : swiperDataList.length,
+              ),
+              constraints: new BoxConstraints.loose(
+                  new Size(MediaQuery.of(context).size.width, 180.0))),
         ],
       ),
       floatingActionButton: FloatingActionButton(
@@ -35,6 +50,10 @@ class Home extends StatelessWidget {
     );
   }
 }
+
+
+
+
 
 
 
