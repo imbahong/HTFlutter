e027d4fd594d61948923bc8e5a7e7524d64e6fd3
hong <imbaTao@outlook.com>
2020-04-28 23:14:24 +0800

    首页变更

--- my_app/lib/main.dart
+++ my_app/lib/main.dart
@@ -1,14 +1,13 @@
 
 import 'package:flutter/material.dart';
 import 'package:my_app/Main/Tabbar/Home/Home.dart';
+import 'package:my_app/pages/index_page.dart';
 
 
 // void main() => runApp(MyApp());
 void main() {
   runApp(new MaterialApp(
     title: 'Flutter Tutorial',
-    home: Home(),
+    home: IndexPage(),
   ));
 }
-
-
